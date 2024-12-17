78431a62efe77040c682fa2ec8b94915d01a2b33
G00433295 <145337306+G00433295@users.noreply.github.com>
2024-12-17 14:53:48 +0000

    Final

--- src/ie/atu/gamingconsole/GamingConsoleManager.java
+++ src/ie/atu/gamingconsole/GamingConsoleManager.java
@@ -39,5 +39,56 @@ public class GamingConsoleManager {
         // Return total number of Gamingconsole objects
         return total;
     }
+    // Method to delete a GamingConsole object by its ID
+    public boolean deleteGamingConsole(int gamingConsoleId) {
+        // Loop through the array to find the GamingConsole with the given ID
+        for (int i = 0; i < Gamingconsoles.length; i++) {
+            // If the GamingConsole is found with the matching ID
+            if (Gamingconsoles[i] != null && Gamingconsoles[i].getGamingconsoleId() == gamingConsoleId) {
+                // Shift elements to the left to fill the gap
+                for (int j = i; j < Gamingconsoles.length - 1; j++) {
+                    Gamingconsoles[j] = Gamingconsoles[j + 1]; // Shift elements left
+                }
+                // Nullify the last slot since it's now a duplicate after shifting
+                Gamingconsoles[Gamingconsoles.length - 1] = null;
+                return true; // Return true to indicate successful deletion
+            }
+        }
+        return false; // Return false if no GamingConsole with the given ID was found
+    }
 
+        // Method to search for a GamingConsole by its ID
+    public GamingConsole searchGamingConsoleById(int gamingConsoleId) {
+        // Loop through array to find the GamingConsole with the given ID
+        for (int i = 0; i < Gamingconsoles.length; i++) {
+            if (Gamingconsoles[i] != null && Gamingconsoles[i].getGamingconsoleId() == gamingConsoleId) {
+                return Gamingconsoles[i]; // Return GamingConsole if found
+            }
+        }
+        return null; // Return null if no GamingConsole with the given ID is found
+    }
+
+    // Method to view all GamingConsoles
+    public void viewAllGamingConsoles() {
+        // Check if the array has any non-null objects
+        boolean hasConsoles = false;
+
+        // Loop through all gaming consoles and display them
+        for (GamingConsole console : Gamingconsoles) {
+            if (console != null) {
+                // Display console details
+                System.out.println("ID: " + console.getGamingconsoleId());
+                System.out.println("Console: " + console.getConsole());
+                System.out.println("Price: " + console.getPrice());
+                System.out.println("Supports VR: " + console.getIssupportsVR());
+                System.out.println("------------");
+                hasConsoles = true;
+            }
+        }
+
+        // If no consoles were found, display a message
+        if (!hasConsoles) {
+            System.out.println("No GamingConsoles available.");
+        }
+    }
 }
--- src/ie/atu/gamingconsole/Main.java
+++ src/ie/atu/gamingconsole/Main.java
@@ -21,11 +21,13 @@ public class Main {
             System.out.println("#     GamingColsole Manager v0.9  #");
             System.out.println("###################################");
             System.out.println("(1) Add a GamingConsole.");
-            System.out.println("(2) Delete a GamingConsole");
+            System.out.println("(2) Delete a GamingConsole.");
             System.out.println("(3) Show total number of GamingConsoles.");
-            System.out.println("(4) Search for GamingConsole by Id");
-            System.out.println("(5) Quit.");
-            System.out.println("Select an option from 1 to 5 and press Enter>");
+            System.out.println("(4) Search for GamingConsole by Id.");
+            System.out.println("(5) View all console options.");
+            System.out.println("(6) View basket.");
+            System.out.println("(7) Quit.");
+            System.out.println("Select an option from 1 to 7 and press Enter>");
 
             // Store user Menu option selection
             userSelection = userInput.nextInt();
@@ -35,7 +37,7 @@ public class Main {
                 // Ask user to enter GamingConsole details
                 System.out.println("Enter GamingConsole Id:");
                 int gamingconsoleId = userInput.nextInt();
-                System.out.println("Enter GamingConsole console:");
+                System.out.println("Enter GamingConsole console from console options:");
                 String console = userInput.next();
                 System.out.println("Enter GamingConsole Price:");
                 float priceEur = userInput.nextFloat();
@@ -47,20 +49,73 @@ public class Main {
                  GamingconsoleManagerObject.addGamingConsole(myGamingConsole);
             } // end if
 
-            // If user selects option 3
-            if (userSelection == 3) {
-                // Display total number of GamingConsole objects
-                System.out.println("Total Number of GamingConsoles: " + GamingconsoleManagerObject.totalGamingConsoles());
+            // If user selects option 2
+            else if (userSelection == 2) {
+                System.out.println("Enter the ID of the GamingConsole you want to delete:");
+                int consoleIdToDelete = userInput.nextInt();
 
-                if (userSelection == 5) {// Quit
+                boolean isDeleted = GamingconsoleManagerObject.deleteGamingConsole(consoleIdToDelete);
 
-                    System.out.println("GamingConsole Manager Closing - Goodbye!");
-                    // Close the User Input Scanner
-                    userInput.close();
-                    break;
-                     // Break out of display menu Infinite loop
-
+                if (isDeleted) {
+                    System.out.println("GamingConsole with ID " + consoleIdToDelete + " has been deleted.");
+                } else {
+                    System.out.println("No GamingConsole found with ID " + consoleIdToDelete);
                 }
+            }
+
+           // If user selects option 3 (Show total number of GamingConsoles)
+           else if (userSelection == 3) {
+            // Display total number of GamingConsole objects
+            System.out.println("Total Number of GamingConsoles: " + GamingconsoleManagerObject.totalGamingConsoles());
+    }
+
+             // If user selects option 4 (Search for GamingConsole by Id)
+             else if (userSelection == 4) {
+                System.out.println("Enter the ID of the GamingConsole you want to search for:");
+                int consoleIdToSearch = userInput.nextInt();
+
+                // Call the search method in GamingConsoleManager
+                GamingConsole foundConsole = GamingconsoleManagerObject.searchGamingConsoleById(consoleIdToSearch);
+
+                // If a console is found, display its details, else inform the user it's not found
+                if (foundConsole != null) {
+                    System.out.println("GamingConsole found:");
+                    System.out.println("ID: " + foundConsole.getGamingconsoleId());
+                    System.out.println("Console: " + foundConsole.getConsole());
+                    System.out.println("Price: " + foundConsole.getPrice());
+                    System.out.println("Supports VR: " + foundConsole.getIssupportsVR());
+                } else {
+                    System.out.println("No GamingConsole found with ID " + consoleIdToSearch);
+                }
+            }
+
+             // If user selects option 5 (View all console options)
+            else if (userSelection == 5) {
+                System.out.println("Available console options:");
+                System.out.println("1. Xbox");
+                System.out.println("2. PlayStation");
+                System.out.println("3. Nintendo Switch");
+            }
+
+
+
+         // If user selects option 7 (Quit)
+         else if (userSelection == 7) { // quit
+            System.out.println("GamingConsole Manager Closing - Goodbye!");
+            // Close the User Input Scanner
+            userInput.close();
+            break; // Exit the loop
+        }
+        // If user selects option 7 (View all GamingConsoles)
+        else if (userSelection == 6) {
+            System.out.println("Displaying all GamingConsoles:");
+
+            // Call the method to display all GamingConsoles
+            GamingconsoleManagerObject.viewAllGamingConsoles();
+        }
+        // If the user selects an invalid option
+        else {
+            System.out.println("Invalid option. Please select a number between 1 and 7.");
             }
         }
     }
